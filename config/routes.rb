40a65863ddc6bdd3cadd7ff0084ff8d807ae900f
bluepostit <32864499+bluepostit@users.noreply.github.com>
2020-02-20 10:46:28 +0200

    ajax actions!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :restaurants, only: %i[show index] do
+  resources :restaurants, only: %i[show index destroy] do
     # POST /restaurants/32/reviews
     resources :reviews, only: :create
   end
