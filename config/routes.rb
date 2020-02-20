5962e0505c1132f5e6d90f46de49725bd5f11d9d
bluepostit <32864499+bluepostit@users.noreply.github.com>
2020-02-20 09:46:53 +0200

    add reviews ctlr, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :restaurants, only: %i[show index]
+  resources :restaurants, only: %i[show index] do
+    # POST /restaurants/32/reviews
+    resources :reviews, only: :create
+  end
 end
